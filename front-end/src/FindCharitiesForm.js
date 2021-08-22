4b66e3d0a281d356994d46ddeb3c90210ac578eb
milanzagorac <zagoracm@gmail.com>
2021-08-22 17:33:35 -0400

    changed a couple of things - start implementing form

---
+++ front-end/src/FindCharitiesForm.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const FindCharitiesForm = () => {
+    return (
+        <div>
+            <form>
+                <label></label>
+                <input />
+            </form>
+        </div>
+    );
+};
+
+export default FindCharitiesForm;
