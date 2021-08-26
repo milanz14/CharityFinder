74038149c435a607fc251d977c134b8b8e90df54
milanzagorac <zagoracm@gmail.com>
2021-08-26 18:41:12 -0400

    added navbar

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,9 +1,11 @@
 import MainContainer from "./MainContainer";
+import Navbar from "./Navbar";
 import "../styles/App.css";
 
 function App() {
     return (
         <div className="App">
+            <Navbar />
             <MainContainer />
         </div>
     );
