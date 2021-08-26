b04d126d939252b8280c4f73506e94c69a1e5fe4
milanzagorac <zagoracm@gmail.com>
2021-08-25 20:52:46 -0400

    try new API

---
+++ front-end/src/components/MainContainer.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const {
+    pledge_api_key,
+    PLEDGE_ORGANIZATIONS_ENDPOINT,
+} = require("../utilities/config");
+
+const MainContainer = () => {
+    const headers = {
+        Authorization: `Bearer ${pledge_api_key}`,
+    };
+
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch();
+    }, []);
+
+    const fetch = () => {
+        axios
+            .get(PLEDGE_ORGANIZATIONS_ENDPOINT, { headers })
+            .then((resp) => console.log(resp))
+            .catch((err) => console.log(err));
+    };
+
+    return (
+        <div>
+            <div>This is the charity form</div>
+        </div>
+    );
+};
+
+export default MainContainer;
