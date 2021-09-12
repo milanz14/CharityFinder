5881190d49881a78cd522625ba30c20060d26a52
milanzagorac <zagoracm@gmail.com>
2021-09-11 20:27:46 -0400

    working on google auth

--- back-end/app.js
+++ back-end/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const ExpressError = require("./ExpressError");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+require("dotenv").config();
 
 // routes --> separate file for Goole auth routes
 
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
 
 app.use((req, res, next) => {
     const e = new ExpressError("Page Not Found", 404);
